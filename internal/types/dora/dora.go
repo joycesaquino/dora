9e4630aac35a9958c773e2f2587764e6c89a8641
joycesaquino <joycesaquino@gmail.com>
2022-10-10 22:41:50 -0300

    Implementing DORA event model

---
+++ internal/types/dora/dora.go
@@ -0,0 +1,32 @@
+package dora
+
+import "time"
+
+type User struct {
+	Login string `json:"login"`
+	ID    int    `json:"id"`
+}
+
+type Dora struct {
+	Action     string `json:"action"`
+	User       User   `json:"user"`
+	Repository struct {
+		ID       int    `json:"id"`
+		Name     string `json:"name"`
+		FullName string `json:"full_name"`
+		Owner    User   `json:"owner"`
+	}
+	PullRequest struct {
+		ID                 int           `json:"id"`
+		URL                string        `json:"url"`
+		Title              string        `json:"title"`
+		Assignee           interface{}   `json:"assignee"`
+		RequestedReviewers []interface{} `json:"requested_reviewers"`
+		RequestedTeams     []interface{} `json:"requested_teams"`
+		AuthorAssociation  string        `json:"author_association"`
+		CreatedAt          time.Time     `json:"created_at"`
+		UpdatedAt          time.Time     `json:"updated_at"`
+		ClosedAt           interface{}   `json:"closed_at"`
+		MergedAt           interface{}   `json:"merged_at"`
+	}
+}
