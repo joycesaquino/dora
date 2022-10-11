9dbb1838a01917e58b5615e8ec7c298f9cc4057d
joycesaquino <joycesaquino@gmail.com>
2022-10-10 22:54:59 -0300

    Implementing dora repository

---
+++ internal/repository/dora.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"context"
+	"dora/internal/dao"
+	types "dora/internal/types/dora"
+)
+
+type DoraRepository struct {
+	dao *dao.Database
+}
+
+type DoraRepositoryInterface interface {
+	Create(ctx context.Context, customer *types.Dora) (*types.Dora, error)
+}
+
+func (repository DoraRepository) Create() {
+}
+
+func NewDoraRepository(ctx context.Context) (*DoraRepository, error) {
+	db, err := dao.NewDatabase(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DoraRepository{
+		dao: db,
+	}, nil
+}
